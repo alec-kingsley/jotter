553b769a3286b85f4c7d77aee5ffe9e96fad4f72
Alec Kingsley <aleckingsley@gmail.com>
2024-12-19 15:18:47 -0500

    improved Expression ops ; drafted flatten skeleton

--- src/definitions.rs
+++ src/definitions.rs
@@ -13,8 +13,8 @@
 
 use regex::Regex;
 use std::collections::HashMap;
-use std::fmt::Display;
 use std::fmt;
+use std::fmt::Display;
 use std::ops::*;
 
 #[derive(Debug, Clone, PartialEq)]
@@ -37,25 +37,48 @@ pub struct Expression {
     pub operators: Vec<String>,
 }
 
+impl Expression {
+    /// "flatten" the `Expression`.
+    ///
+    /// remove a many parentheticals as possible, such that it's just a sum of terms.
+    /// combine like terms.
+    ///
+    fn flatten(&mut self) {
+        for _self_i in 0..self.operands.len() {
+            // TODO - multiply/divide through the factors in the term
+
+            // TODO - if it's a singular parenthetical {
+
+            // TODO - flatten contents of parenthetical
+
+            // TODO - add flattened paranthetical contents to father expression
+
+            // } TODO - else if it has a parenthetical denominator {
+            
+            // TODO - flatten contents of parenthetical denominator
+
+            // }
+        }
+    }
+}
+
 impl Add for Expression {
     type Output = Self;
 
     /// Operator overload for +.
     ///
     fn add(self, other: Self) -> Self {
-        Self {
-            operands: vec![
-                Term {
-                    operands: vec![Factor::Parenthetical(self.clone())],
-                    operators: Vec::new(),
-                },
-                Term {
-                    operands: vec![Factor::Parenthetical(other.clone())],
-                    operators: Vec::new(),
-                },
-            ],
-            operators: vec![String::from("+")],
+        assert!(self.operands.len() > 0);
+        assert!(other.operands.len() > 0);
+        let mut result = self.clone();
+        for operand in &other.operands {
+            result.operands.push(operand.clone());
         }
+        result.operators.push(String::from("+"));
+        for operator in &other.operators {
+            result.operators.push(operator.clone());
+        }
+        result
     }
 }
 
@@ -73,19 +96,21 @@ impl Sub for Expression {
     /// Operator overload for -.
     ///
     fn sub(self, other: Self) -> Self {
-        Self {
-            operands: vec![
-                Term {
-                    operands: vec![Factor::Parenthetical(self.clone())],
-                    operators: Vec::new(),
-                },
-                Term {
-                    operands: vec![Factor::Parenthetical(other.clone())],
-                    operators: Vec::new(),
-                },
-            ],
-            operators: vec![String::from("-")],
+        assert!(self.operands.len() > 0);
+        assert!(other.operands.len() > 0);
+        let mut result = self.clone();
+        for operand in &other.operands {
+            result.operands.push(operand.clone());
         }
+        result.operators.push(String::from("-"));
+        for operator in &other.operators {
+            result.operators.push(if operator == "-" {
+                String::from("+")
+            } else {
+                String::from("-")
+            });
+        }
+        result
     }
 }
 
@@ -103,16 +128,34 @@ impl Mul for Expression {
     /// Operator overload for *.
     ///
     fn mul(self, other: Self) -> Self {
-        Self {
-            operands: vec![Term {
-                operands: vec![
-                    Factor::Parenthetical(self.clone()),
-                    Factor::Parenthetical(other.clone()),
-                ],
-                operators: vec![String::from("*")],
-            }],
+        let mut result = Expression {
+            operands: Vec::new(),
             operators: Vec::new(),
+        };
+        for self_i in 0..self.operands.len() {
+            for other_i in 0..other.operands.len() {
+                result
+                    .operands
+                    .push(self.operands[self_i].clone() * other.operands[other_i].clone());
+                result.operators.push(
+                    // determine if it should be added or subtracted
+                    if (if self_i > 0 {
+                        self.operators[self_i - 1].clone()
+                    } else {
+                        String::from("+")
+                    }) == (if other_i > 0 {
+                        other.operators[other_i - 1].clone()
+                    } else {
+                        String::from("+")
+                    }) {
+                        String::from("+")
+                    } else {
+                        String::from("-")
+                    },
+                )
+            }
         }
+        result
     }
 }
 
@@ -129,17 +172,18 @@ impl Div for Expression {
 
     /// Operator overload for /.
     ///
+    /// Just take every term and divide it by `other` thrown in a parenthetical.
+    /// Don't wanna think about math. Dividing by an expression is hard.
+    ///
     fn div(self, other: Self) -> Self {
-        Self {
-            operands: vec![Term {
-                operands: vec![
-                    Factor::Parenthetical(self.clone()),
-                    Factor::Parenthetical(other.clone()),
-                ],
-                operators: vec![String::from("/")],
-            }],
-            operators: Vec::new(),
+        let mut result = self.clone();
+        for self_i in 0..self.operands.len() {
+            result.operands[self_i]
+                .operands
+                .push(Factor::Parenthetical(other.clone()));
+            result.operands[self_i].operators.push(String::from("/"));
         }
+        result
     }
 }
 
@@ -158,6 +202,52 @@ pub struct Term {
     pub operators: Vec<String>,
 }
 
+impl Neg for Term {
+    type Output = Self;
+
+    fn neg(self) -> Self {
+        assert!(self.operands.len() > 0);
+        let mut result = self.clone();
+        result.operators.push(String::from("*"));
+        result.operands.push(Factor::Number(Number {
+            value: -1f64,
+            unit: Unit {
+                exponent: 0,
+                constituents: HashMap::new(),
+            },
+        }));
+        result
+    }
+}
+
+impl Mul for Term {
+    type Output = Self;
+
+    /// Operator overload for *.
+    ///
+    fn mul(self, other: Self) -> Self {
+        let mut result = self.clone();
+        for operand in &other.operands {
+            result.operands.push(operand.clone());
+        }
+        if result.operators.len() > 0 {
+            result.operators.push(String::from("*"));
+        }
+        for operator in &other.operators {
+            result.operators.push(operator.clone());
+        }
+        result
+    }
+}
+
+impl MulAssign for Term {
+    /// Operator overload for *=.
+    ///
+    fn mul_assign(&mut self, other: Self) {
+        self.clone_from(&(self.clone() * other));
+    }
+}
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum Factor {
     Parenthetical(Expression),
@@ -172,22 +262,52 @@ impl Mul for Factor {
     /// Operator overload for *.
     ///
     fn mul(self, other: Self) -> Self {
-        // TODO - implement function
-
-        // NOTE - leave Number, Identifier, and Call alone
-        // distribute parentheticals
-        // return Number if both inputs are Numbers
-        // else return Parenthetical
-
         let mut result: Option<Factor> = None;
-        if let Factor::Number(number) = self {
-            if let Factor::Number(other_number) = other {
-                result = Some(Factor::Number(number * other_number));
+        if let Factor::Number(self_number) = self.clone() {
+            if let Factor::Number(other_number) = other.clone() {
+                result = Some(Factor::Number(self_number * other_number));
             }
         }
-
-        result.expect("Failed to multiply factors")
-
+        if result == None {
+            result = Some(
+                if let Factor::Parenthetical(self_expression) = self.clone() {
+                    if let Factor::Parenthetical(other_expression) = other.clone() {
+                        Factor::Parenthetical(self_expression * other_expression)
+                    } else {
+                        Factor::Parenthetical(
+                            self_expression
+                                * Expression {
+                                    operands: vec![Term {
+                                        operands: vec![other],
+                                        operators: Vec::new(),
+                                    }],
+                                    operators: Vec::new(),
+                                },
+                        )
+                    }
+                } else if let Factor::Parenthetical(other_expression) = other.clone() {
+                    Factor::Parenthetical(
+                        other_expression
+                            * Expression {
+                                operands: vec![Term {
+                                    operands: vec![other],
+                                    operators: Vec::new(),
+                                }],
+                                operators: Vec::new(),
+                            },
+                    )
+                } else {
+                    Factor::Parenthetical(Expression {
+                        operands: vec![Term {
+                            operands: vec![self, other],
+                            operators: vec![String::from("*")],
+                        }],
+                        operators: Vec::new(),
+                    })
+                },
+            );
+        }
+        result.unwrap()
     }
 }
 
@@ -431,4 +551,3 @@ mod tests {
         let _ = Identifier::new("-").unwrap_err();
     }
 }
-
